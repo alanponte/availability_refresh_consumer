91d7ad1b3c8160db5aac7c9a37a67bcbff3ea77e
Alan Ponte <103460929+alanponte@users.noreply.github.com>
2023-11-07 10:17:18 -0800

    remove old commend

--- message/appointment.py
+++ message/appointment.py
@@ -14,25 +14,7 @@ class AppointmentCanceledMessage(RootMessage):
 
 @dataclass(frozen=True)
 class AppointmentScheduledMessage(RootMessage):
-    """Message to send to SNS when an appointment has been created.
-
-    Example:
-
-    ```
-        {
-            user_id: 'e863b41f-acdc-4eff-93eb-b47a60879dec',
-            fleet_id: 'e35396fb-053e-4479-b7e2-2ff207073485',
-            store_id: '4fe35389-f3fe-4996-ab48-86e5225795f9',
-            service_windows: {
-                start_time: 1,
-                service_completion_time: 2
-                end_time:3,
-                jobs: [job1, job2, ..],
-                availability_asset: '487792fe-b115-4889-9299-2404f7e2807d'
-            }
-        }
-    ```
-    """
+    """Message to send to SNS when an appointment has been created."""
     user_id: str
     fleet_id: str
     store_id: str
